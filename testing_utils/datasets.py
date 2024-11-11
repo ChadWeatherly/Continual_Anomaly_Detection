5f49133a30bcb3f29554d723566d434575b80e0f
Chad Weatherly <chadweatherly2@gmail.com>
2024-11-11 17:02:20 -0600

    updates so far

--- testing_utils/datasets.py
+++ testing_utils/datasets.py
@@ -9,7 +9,7 @@ from torchvision.models import ViT_B_16_Weights
 
 
 class mvtec(Dataset):
-    def __init__(self, multi=True, mtd=False, train=True, task=None, cutpaste=False):
+    def __init__(self, multi=True, mtd=False, train=True, task=None):
         """
         Creates MVTEC dataset for use in PyTorch. Based on Towards Continaul Anomaly Detection paper
         Args:
@@ -30,20 +30,14 @@ class mvtec(Dataset):
         self.get_task_categories()  # creates self.categories, containing all needed category labels in a list
         self.filenames = None
         self.labels = None  # 1 = anomaly, 0 = good
-        self.get_all_filenames()  # creates list of all filenames and paths
-        self.cutpaste = cutpaste  # Whether to use cutpaste transforms or not
-        vit_transforms = ViT_B_16_Weights.DEFAULT.transforms()  # Pre-processing transforms for ViT (resize, normalize)
-        self.pre_transforms = transforms.Compose([
-            vit_transforms,
-            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-        ])
-        self.post_transforms = transforms.Compose([
-            transforms.RandomRotation(90),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        if cutpaste:
+        self.get_all_filenames()  # creates list of all filenames and paths, self.filenames
+        self.vit_transforms = ViT_B_16_Weights.DEFAULT.transforms()  # Pre-processing transforms for ViT (resize, normalize)
+        if train:
             # These transforms are copied from the original code, which does them sneakily without telling you
             self.cutpaste_transform = CutPaste()
+            self.transform = transforms.Compose([
+                transforms.RandomRotation(90)
+            ])
 
         return
 
@@ -90,7 +84,7 @@ class mvtec(Dataset):
         return
 
     def __len__(self):
-        if self.cutpaste:
+        if self.train:
             return len(self.filenames) * 2
         else:
             return len(self.filenames)
@@ -98,22 +92,21 @@ class mvtec(Dataset):
     def __getitem__(self, idx):
         # Images need to be pre-processed beforehand so dataloader handles same size images
         # need to do is make sure the image has 3 channels
-        if self.cutpaste:
+        if self.train:
             label = 1 if idx >= len(self.filenames) else 0
             new_idx = idx % len(self.filenames)
             category = self.filenames[new_idx].split('/')[1]
             img = read_image(self.filenames[new_idx]).expand(3, -1, -1)
-            # Transforms the image from 3xHxW to 3x224x224 and normalizes all values to [0, 1]
-            img = self.pre_transforms(img)  # B x 3 x 224 x 224
             if label == 1:
                 img = self.cutpaste_transform(img)
-            img = self.post_transforms(img)
+            img = self.transform(img)
+            img = self.vit_transforms(img)
             return img, label, category
-        else:
+        else:  # For testing mode
             category = self.filenames[idx].split('/')[1]
             img = read_image(self.filenames[idx]).expand(3, -1, -1)
             # Transforms the image from 3xHxW to 3x224x224 and normalizes all values to [0, 1]
-            img = self.post_transforms(self.pre_transforms(img))  # B x 3 x 224 x 224
+            img = self.vit_transforms(img)  # B x 3 x 224 x 224
             return img, self.labels[idx], category
 
 
@@ -124,8 +117,6 @@ class CutPaste(object):
         This class creates to different augmentation CutPaste and CutPaste-Scar. Moreover, it returns augmented images
         for binary and 3 way classification
 
-        :arg
-        :transform[binary]: - if True use Color Jitter augmentations for patches
         '''
         self.to_pil = transforms.ToPILImage()
         self.to_tensor = transforms.Compose([
@@ -141,7 +132,6 @@ class CutPaste(object):
         :image: [PIL] _ original image
         :patch_w: [int] _ width of the patch
         :patch_h: [int] _ height of the patch
-        :transform: [binary] _ if True use Color Jitter augmentation
         :rotation: [binary[ _ if True randomly rotates image from (-45, 45) range
 
         :return: augmented image
